f2253ea172d620d1556980a87bfc299ee4a849a0
Eaeajj <ozalesskiy71@gmail.com>
2024-08-02 18:57:05 +0300

    2.60

---
+++ racket-src/chapter 2/2.3 Symbolic Data/2.3.3 Example: Representing Sets/2.60.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(require rackunit)
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (remove-set-element x set)
+  (cond ((null? set) '())
+        ((equal? (car set) x) (remove-set-element x (cdr set)))
+        (else (cons (car set) (remove-set-element x (cdr set))))))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) (remove-set-element (car set1) set2))))
+        (else (intersection-set (cdr set1) set2))))
+
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+(check-equal? (remove-set-element 4 '(4 2 3 4 )) '(2 3))
+; (check-equal? (union-set '(1 2 3) '(2 3 4)) '(1 2 3 4))
